7477d452cd236cdf3a96622bc46882496b2ff526
Chase <chasewinder912@gmail.com>
2021-05-12 17:17:20 +1200

    added multiplication, division and subtraction

--- python_files/calculator.py
+++ python_files/calculator.py
@@ -3,13 +3,23 @@ def add(value_1, value_2):
     sum = value_1 + value_2
     return sum
 
-a = float(input("Enter value one: "))
-b = float(input("Enter value two: "))
-
-print(add(a, b))
-
 def subtraction(value_1, value_2):
     difference = value_1 - value_2
     return difference
 
+
+def multiplication(value_1, value_2):
+    product = value_1 * value_2
+    return product
+
+def division(value_1, value_2):
+    quotient = value_1 / value_2
+    return quotient
+
+a = float(input("Enter value one: "))
+b = float(input("Enter value two: "))
+
+print(add(a, b))
 print(subtraction(a, b))
+print(multiplication(a, b))
+print(division(a, b))
