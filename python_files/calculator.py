00acf5354416bf6ba3c0c085e5479dd79b52c98a
Chase <chasewinder912@gmail.com>
2021-05-12 17:05:35 +1200

    basic subtraction function added

--- python_files/calculator.py
+++ python_files/calculator.py
@@ -7,3 +7,9 @@ a = float(input("Enter value one: "))
 b = float(input("Enter value two: "))
 
 print(add(a, b))
+
+def subtraction(value_1, value_2):
+    difference = value_1 - value_2
+    return difference
+
+print(subtraction(a, b))
